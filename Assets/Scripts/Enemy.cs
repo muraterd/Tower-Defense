1a7f1fa93a0efde5a9f22dc205133ba51b1ad287
Murat Erdoğan <murat@muraterdogan.me>
2017-04-20 17:20:35 +0300

    Enemy and the waypoints added

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int waypointIndex = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        target = Waypoints.points[waypointIndex];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+
+        // Check if the enemy reached the target waypoint
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    private void GetNextWaypoint()
+    {
+        if (waypointIndex >= Waypoints.points.Count - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        target = Waypoints.points[++waypointIndex];
+    }
+}
--- Assets/Scripts/Waypoints.cs
+++ Assets/Scripts/Waypoints.cs
@@ -6,17 +6,11 @@ public class Waypoints : MonoBehaviour
     public static List<Transform> points = new List<Transform>();
 
     // Use this for initialization
-    void Start()
+    void Awake()
     {
         foreach (Transform child in transform)
         {
             points.Add(child);
-        } 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        }
     }
 }
