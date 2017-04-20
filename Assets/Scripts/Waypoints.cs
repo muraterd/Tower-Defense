f04ec7b330d17ae5642f1d6fdb21aad249516e8d
Murat Erdoğan <murat@muraterdogan.me>
2017-04-20 14:09:08 +0300

    Initial commit

---
+++ Assets/Scripts/Waypoints.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Waypoints : MonoBehaviour
+{
+    public static List<Transform> points = new List<Transform>();
+
+    // Use this for initialization
+    void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            points.Add(child);
+        } 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
