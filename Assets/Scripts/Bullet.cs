89a3416008c2e6f86773039379cdcbfdbd2cb9f4
Murat Erdoğan <murat@muraterdogan.me>
2017-04-28 15:33:38 +0300

    Hit the target

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour
+{
+
+    private Transform target;
+
+    public float speed = 40f;
+
+    void Start()
+    {
+
+    }
+
+    public void SetTarget(Transform _target)
+    {
+        target = _target;
+    }
+
+    void Update()
+    {
+        if (target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        // How far the bullt travelled
+        float distanceTravelled = speed * Time.deltaTime;
+
+        if (dir.magnitude <= distanceTravelled)
+        {
+            HitTarget();
+        }
+    }
+
+    void HitTarget()
+    {
+        Destroy(gameObject);
+        Destroy(target.gameObject);
+        return;
+    }
+}
--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -3,16 +3,23 @@ using System.Collections;
 
 public class Turret : MonoBehaviour
 {
-
     private Transform target;
+
+    [Header("Attributes")]
     public float range = 15f;
+    public float turnSpeed = 10f;
+    public float fireRate = 1f;
+
+    [Header("Unity Setup Fileds")]
     public string enemyTag = "Enemy";
     public Transform partToRotate;
-    public float turnSpeed = 10f;
+    public GameObject bulletPrefab;
+    public Transform shootingPoint;
 
     void Start()
     {
         InvokeRepeating("SearchTarget", 0f, 0.5f);
+        InvokeRepeating("Shoot", 0f, 1 / fireRate);
     }
 
     void SearchTarget()
@@ -52,6 +59,16 @@ public class Turret : MonoBehaviour
         Quaternion lookRotation = Quaternion.LookRotation(dir);
         Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
         partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+    }
+
+    void Shoot()
+    {
+        if (target != null)
+        {
+            GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, shootingPoint.position, shootingPoint.rotation);
+            Bullet bullet = bulletGO.GetComponent<Bullet>();
+            bullet.SetTarget(target);
+        }
     }
 
     void OnDrawGizmosSelected()
